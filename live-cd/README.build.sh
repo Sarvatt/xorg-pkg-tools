a761b5330918064497a6821818ac4d91c02be53e
Tormod Volden <debian.tormod@gmail.com>
2009-06-02 23:42:32 +0200

    live-cd/README.build.sh updated about kernel and headers

--- live-cd/README.build.sh
+++ live-cd/README.build.sh
@@ -12,11 +12,15 @@ the "edit" and "extract-cd" directories. If the script detects these
 directories on later runs, it will not look for and extract the
 original image.
 
+Any linux*.deb kernel (and -headers) packages will be installed and
+replace the original kernel.
+
 An "xorg-edgers-bg.png" file in the current directory will be copied to 
 the CD image and used as a desktop background.
 
-An "xorg-edgers.rc" file can be added to the cd root and will be called
+An "xorg-edgers.rc" file can be added to the CD root and will be called
 by /etc/rc.local. This can be useful for making local tweaks to the
-"live CD" if the cd is copied to a USB stick, since the xorg-edgers.rc
+"live CD" if the CD is copied to a USB stick, since the xorg-edgers.rc
 file can be easily modified without rebuilding the squashfs.
 
+Any *.README files will be copied to the CD root.
