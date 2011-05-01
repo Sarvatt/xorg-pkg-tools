736e9a4fb0ae1bec03932d370a52414db464c48b
Christopher James Halse Rogers <raof@ubuntu.com>
2011-05-02 09:37:40 +1000

    rebuild-all-drivers.sh: Update for new ABI policy, add versioned build-depends when rebuilding

--- rebuild-all-drivers.sh
+++ rebuild-all-drivers.sh
@@ -6,11 +6,6 @@
 # Copyright 2011 Canonical, Ltd.
 # Authors:  Chris Halse Rogers, Bryce Harrington
 
-# TODO: Make *sure* to wait until all of the xserver builds are done
-#       (Including arm and everything else)
-
-# TODO: Don't forget about indirect dependencies of -video-all, such as -mach64 and -r128
-
 # TODO: Stage all this in a xorg-staging PPA
 
 # Verify system is the current development ubuntu (requires python-launchpadlib-toolkit)
@@ -21,26 +16,35 @@ if [ $(lsb_release -cs) != "$codename" ] ; then
     exit 1
 fi
 
-X_INPUT_ABI=$(cut -d ',' -f 1 /usr/share/xserver-xorg/xinputdep 2> /dev/null)
-X_VIDEO_ABI=$(cut -d ',' -f 1 /usr/share/xserver-xorg/xvideodep 2> /dev/null)
+X_INPUT_ABI=$(cut -d, -f 1 /usr/share/xserver-xorg/inputabiver)
+X_VIDEO_ABI=$(cut -d, -f 1 /usr/share/xserver-xorg/videoabiver)
 
 if [ -z "$X_INPUT_ABI" || -z "$X_VIDEO_ABI" ] ; then
     echo "Current xserver-xorg-dev package must be installed"
     exit 1
 fi
 
-apt-get source $(grep-aptavail -F Depends -s Source:Package $X_VIDEO_ABI | sed s/Source: //g)
-apt-get source $(grep-aptavail -F Depends -s Source:Package $X_INPUT_ABI | sed s/Source: //g)
+if [ -z "$1" ] ; then
+    echo "Must specify new minimum xserver version"
+    exit 1
+fi
+
+# Grab all the packages depending on the current Xserver video and input ABI.
+# There may be some packages which don't have correct dependencies, but there's not much we can do about them.
+apt-get source $(grep-aptavail -F Depends -s Source:Package xorg-video-abi-$X_VIDEO_ABI | sed "s/Source: //g")
+apt-get source $(grep-aptavail -F Depends -s Source:Package xorg-input-abi-$X_INPUT_ABI | sed "s/Source: //g")
+
+rm *.dsc
 
 for I in * ; do
-   [ -d $I ] && (cd $I && dch --rebuild "Rebuild against Xserver 1.10" --distribution ${codename} && dpkg-buildpackage -S)
+   [ -d $I ] && (cd $I &&
+# Add versioned build-dependency on new Xserver.
+       sed -i -r "s/xserver-xorg-dev([[:space:]]*\(>=? 2:1(\.[[:digit:]]*)*([~+]git[[:digit:]]*(\.(.){8})?)?~?(-[[:digit:]]+(ubuntu[[:digit:]]+)?~?)?\))?/xserver-xorg-dev (>= $1)/g" debian/control
+       dch --rebuild "Rebuild to pick up new Xserver dependencies" --distribution ${codename} &&
+       dpkg-buildpackage -S)
 done
 
 echo "All *.changes ready to be uploaded"
-
-#<vorlon> bryyce: well, armel got hit by the no-change driver rebuilds not having versioned build-dependencies on the new xserver# -dev... :)
-# bryyce: when you're doing such ABI rebuilds, could you please bump the versioned build-dep?
-# TODO:  Above seems hard to do automatically, and would require parsing/rewriting the control file...
 
 # TODO: Pause and let the user doublecheck things
 #for I in *.changes ; do
